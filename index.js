c72d529217e6ab4cd3c531d65fe6197fcdd08e9b
Bianca <bianca.costamelo1@gmail.com>
2025-02-22 20:04:25 -0300

    adicionando funcionalidade para trocar de tema (escuro/claro); melhorando responsividade; melhorando UI

--- index.js
+++ index.js
@@ -1,53 +1,52 @@
-const ampla = document.getElementById("ampla")
-const padrao = document.getElementById("padrao")
-const compacto = document.getElementById("compacto")
-const cardProjeto = document.querySelectorAll(".cardProjeto")
-const imgProjeto = document.querySelectorAll(".imgContainer")
-const videoContent = document.querySelectorAll(".videoContent")
+const ampla = document.getElementById("ampla");
+const padrao = document.getElementById("padrao");
+const compacto = document.getElementById("compacto");
+const cardProjeto = document.querySelectorAll(".cardProjeto");
+const imgContainer = document.querySelectorAll(".imgContainer");
+const videoContent = document.querySelectorAll(".videoContent");
 
-ampla.addEventListener('click', () => {
-    if (ampla) {
-        imgProjeto.forEach((e) => {
-            e.style.height = '280px'
-        })
-        
-        cardProjeto.forEach((e) => {
-            e.style.width = '570px'
-        })
-        videoContent.forEach((e) => {
-            e.style.width = '100%'
-            e.style.height = 'auto' 
-        })
-    }
-})
+ampla.addEventListener("click", () => {
+  imgContainer.forEach((e) => {
+    e.style.height = "280px";
+  });
 
-padrao.addEventListener('click', () => {
-    if (padrao) {
-        imgProjeto.forEach((e) => {
-            e.style.height = '200px'
-        })
-        
-        cardProjeto.forEach((e) => {
-            e.style.width = '400px'
-        })
-        videoContent.forEach((e) => {
-            e.style.width = '100%'
-            e.style.height = 'auto' 
-        })
-    }
-})
+  cardProjeto.forEach((e) => {
+    e.style.width = "570px";
+  });
+  videoContent.forEach((e) => {
+    e.style.width = "100%";
+    e.style.height = "auto";
+  });
+});
 
-compacto.addEventListener('click', () => {
-    if (compacto) {
-        imgProjeto.forEach((e) => {
-            e.style.height = '160px'
-        })
-        
-        cardProjeto.forEach((e) => {
-            e.style.width = '295px'
-        })
-        videoContent.forEach((e) => {
-            e.style.height = '100%' 
-        })
-    }
-})
+padrao.addEventListener("click", () => {
+  imgContainer.forEach((e) => {
+    e.style.height = "200px";
+  });
+
+  cardProjeto.forEach((e) => {
+    e.style.width = "400px";
+  });
+  videoContent.forEach((e) => {
+    e.style.width = "100%";
+    e.style.height = "auto";
+  });
+});
+
+compacto.addEventListener("click", () => {
+  imgContainer.forEach((e) => {
+    e.style.height = "160px";
+  });
+
+  cardProjeto.forEach((e) => {
+    e.style.width = "295px";
+  });
+  videoContent.forEach((e) => {
+    e.style.height = "100%";
+  });
+});
+
+function mudarTheme() {
+  document.body.classList.toggle("dark-theme");
+  document.body.classList.toggle("light-theme");
+}
